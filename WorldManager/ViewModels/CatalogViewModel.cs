3a2dc352885c3618901b14f76afe2e51bad3d511
silentwasd <silentwasd@gmail.com>
2022-06-17 11:50:24 +0300

    Добавлена сортировка миров. Добавлена сортировка по совместимости Quest. Добавлена сортировка по FEATURED (?). Добавлена информация к выводимым мирам.

--- WorldManager/ViewModels/CatalogViewModel.cs
+++ WorldManager/ViewModels/CatalogViewModel.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using ReactiveUI;
@@ -17,6 +17,12 @@ public class CatalogViewModel : ViewModelBase
 
     private string _search;
 
+    private KeyValuePair<string, string> _sort;
+
+    private bool _quest;
+
+    private bool _featured;
+
     public CatalogViewModel()
     {
         _apiConfig = new Configuration();
@@ -25,17 +31,41 @@ public class CatalogViewModel : ViewModelBase
     public CatalogViewModel(Configuration apiConfig)
     {
         _apiConfig = apiConfig;
-        LoadActiveWorlds();
+
+        Sort = new KeyValuePair<string, string>("popularity", "популярность");
+        Quest = false;
 
         this.WhenAnyValue(x => x.Search)
             .Throttle(TimeSpan.FromSeconds(1))
             .ObserveOn(RxApp.MainThreadScheduler)
             .Subscribe(x =>
             {
-                if (String.IsNullOrWhiteSpace(x))
-                    LoadActiveWorlds();
-                else
+                LoadFoundWorlds();
+            });
+        
+        this.WhenAnyValue(x => x.Featured)
+            .Throttle(TimeSpan.FromSeconds(1))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(x =>
+            {
+                LoadFoundWorlds();
+            });
+        
+        this.WhenAnyValue(x => x.Sort)
+            .Throttle(TimeSpan.FromSeconds(1))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(x =>
+            {
+                if (x.Key != String.Empty)
                     LoadFoundWorlds();
+            });
+        
+        this.WhenAnyValue(x => x.Quest)
+            .Throttle(TimeSpan.FromSeconds(1))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(x =>
+            {
+                LoadFoundWorlds();
             });
     }
 
@@ -55,7 +85,7 @@ public class CatalogViewModel : ViewModelBase
     private void LoadFoundWorlds()
     {
         var worldsApi = new WorldsApi(_apiConfig);
-        var worlds = worldsApi.SearchWorlds(search: Search);
+        var worlds = worldsApi.SearchWorlds(search: Search, featured: Featured.ToString(), sort: Sort.Key, platform: Quest ? "android" : "");
         ActiveWorlds.Clear();
         foreach (var world in worlds)
         {
@@ -78,4 +108,38 @@ public class CatalogViewModel : ViewModelBase
         get => _search;
         set => this.RaiseAndSetIfChanged(ref _search, value);
     }
+    
+    public bool Featured
+    {
+        get => _featured;
+        set => this.RaiseAndSetIfChanged(ref _featured, value);
+    }
+    
+    public KeyValuePair<string, string> Sort
+    {
+        get => _sort;
+        set => this.RaiseAndSetIfChanged(ref _sort, value);
+    }
+    
+    public bool Quest
+    {
+        get => _quest;
+        set => this.RaiseAndSetIfChanged(ref _quest, value);
+    }
+
+    public Dictionary<string, string> SortList => new()
+    {
+        { "popularity", "популярность" },
+        { "heat", "тренд" },
+        { "trust", "доверие" },
+        { "shuffle", "смешанно" },
+        { "random", "случайно" },
+        { "favorites", "избранное" },
+        { "publicationDate", "дата публикации" },
+        { "labsPublicationDate", "дата публикации в лаборатории" },
+        { "order", "порядок" },
+        { "relevance", "релевантность" },
+        { "magic", "магия" },
+        { "name", "название" }
+    };
 }
